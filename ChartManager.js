006ebc2c12e095b6878ca542c9d879857eb8ec39
dontsober <81525855+dontsober@users.noreply.github.com>
2024-09-09 17:26:28 +0100

    Google Cloud

--- ChartManager.js
+++ ChartManager.js
@@ -25,7 +25,7 @@ class ChartManager {
 			initial_investment: initial_investment
         };
         
-        const url = `http://127.0.0.1:8080/calculations`;
+        const url = `https://flask-investment-simulator-2groyyujbq-uc.a.run.app/calculations`;
 
         fetch(url, {
             method: 'POST',
--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     const password = document.getElementById('password').value;
 
     try {
-        const response = await fetch('http://127.0.0.1:8080/login', {
+        const response = await fetch('https://flask-investment-simulator-2groyyujbq-uc.a.run.app/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
