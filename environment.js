e1812af4f04715e000a00f1511bddd71d87990d2
dontsober <81525855+dontsober@users.noreply.github.com>
2024-09-13 08:40:58 +0100

    Ignore

--- environment.js
+++ environment.js
@@ -1,3 +1,3 @@
-const apiUrlAAFE = "https://flask-investment-simulator-2groyyujbq-uc.a.run.app";
-//local http://127.0.0.1:8080
 //const apiUrlAAFE = "https://flask-investment-simulator-2groyyujbq-uc.a.run.app";
+//local http://127.0.0.1:8080
+const apiUrlAAFE = "https://flask-investment-simulator-2groyyujbq-uc.a.run.app";
